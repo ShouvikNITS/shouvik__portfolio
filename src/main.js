6a536bf76640f5fcd9d8ad89e7bcec15bbcd0b26
Shouvik Paul <153314600+ShouvikNITS@users.noreply.github.com>
2024-06-06 23:37:33 +0530

    projects, footer, etc update

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import '../styles/components/header.css';
 import '../styles/components/nav.css';
 import '../styles/components/about.css';
 import '../styles/components/skills.css';
+import '../styles/components/projects.css';
 import '../styles/components/biodata.css';
 import '../styles/components/contact.css';
+import '../styles/components/social.css';
+import '../styles/components/footer.css';
 import '../styles/style.css';
 
 import skillsAnimation from './utils/skills';
